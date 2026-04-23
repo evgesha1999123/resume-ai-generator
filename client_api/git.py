572c80a5409f558c38a472e1585a548e0c9e326c
evgesha1999 <evgesha.22013@gmail.com>
2026-04-24 01:41:06 +0300

    github api client added

--- client_api/git.py
+++ client_api/git.py
@@ -54,7 +54,7 @@ class GitAPIClient:
 
     async def get_project_tree(self, owner: str, repo: str, sha: str, recursive: bool) -> Response:
         return await self.client.get(
-            url=f"{self.base_url}/repos/{owner}/{repo}/git/trees/{sha}?recursive={str(int(recursive))}",
+            url=f"{self.base_url}/repos/{owner}/{repo}/git/trees/{sha}?recursive={int(recursive)}",
             headers=self.__put_headers()
         )
 
@@ -70,22 +70,39 @@ class GitAPIClient:
 async def use_case():
     client = GitAPIClient(access_token=Settings().github_api.ACCESS_TOKEN)
     # response = await client.get_readme(owner="evgesha1999123", repo="resume-ai-generator")
-    for it in range(11):
-        branches_response = await client.get_project_branches("flutter", "engine", pagination_size=100, page=it)
-        response_json = branches_response.json()
-        find_master_branch(response_json)
-    # response = await client.get_project_tree(owner="flutter", repo="engine", sha="70671823931a630cd40c4db50db67a9d7e1367cf", recursive=True)
+    default_branch_sha = await get_default_branch_sha(client)
+
+    project_tree_response = await client.get_project_tree("flutter", "engine", default_branch_sha, True)
+    with open("tree.json", mode="w+", encoding="utf-8") as f:
+        f.write(str(project_tree_response.json()))
+    # pprint(project_tree_response.json())
+
     # print(response.text)
     # print(response.status_code)
     # pprint(response.json())
 
-def find_master_branch(response_json: list[dict[str, Any]]):
+def find_default_branch_sha(response_json: list[dict[str, Any]]) -> str | None:
     for branch in response_json:
         branch_name = branch["name"]
         if branch_name in ["master", "main"]:
-            print(branch["name"])
-            return
-    print("NOT FOUND!")
+            return branch["commit"]["sha"]
+    print("not found, wait")
+    return None
+
+async def get_default_branch_sha(client: GitAPIClient) -> str:
+    default_branch_sha = None
+    current_page = 1
+    while not default_branch_sha:
+        branches_response = await client.get_project_branches(
+            owner="flutter",
+            repo="engine",
+            pagination_size=100,
+            page=current_page
+        )
+        response_json = branches_response.json()
+        default_branch_sha = find_default_branch_sha(response_json)
+        current_page += 1
+    return default_branch_sha
 
 
 if __name__ == '__main__':
